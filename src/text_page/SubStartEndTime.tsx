d0cbd6255c0c4ce74b74ad2d22ac51054a6e86d2
vitaliiboiko360 <vukelan@gmail.com>
2024-01-27 13:45:08 +0200

    work changing between sub and input

--- src/text_page/SubStartEndTime.tsx
+++ src/text_page/SubStartEndTime.tsx
@@ -1,3 +1,4 @@
+import { TrendingUpTwoTone } from '@mui/icons-material';
 import React from 'react'
 import { forwardRef } from 'react';
 
@@ -5,7 +6,7 @@ const SubStartEndTime = forwardRef((props, ref) => {
   const localClassName = (props.value == 0 && props.force != true) ? 'unassigned' : props.className;
   const className = `sub ${localClassName}`;
   return (
-    <sub ref={ref} className={className} contentEditable suppressContentEditableWarning={true}>{props.value}</sub>
+    <sub ref={ref} className={className}>{props.value}</sub>
   );
 });
 
@@ -15,6 +16,7 @@ const SubStartEndTimeInputField = forwardRef((props, ref) => {
   console.log(`we recived props.totalTime ${props.totalTime}`);
   return (
     <input
+      ref={ref}
       className={className}
       type='number'
       maxLength={5} minLength={1}
@@ -24,6 +26,9 @@ const SubStartEndTimeInputField = forwardRef((props, ref) => {
       value={value}
       onChange={(e) => { setValue(e.target.value) }}
       step={0.01}
+      onBlur={(e) => {
+        props.updateValue(value);
+      }}
     // pattern='[0-9]{1,2}\.[0-9]{1,2}'
     // required
     ></input >
@@ -33,16 +38,66 @@ const SubStartEndTimeInputField = forwardRef((props, ref) => {
 export function SubStartEndTimeEditableField(props) {
 
   const subRef = React.useRef(null);
+  const inputRef = React.useRef(null);
+
   const [activeEditMode, setActiveEditMode] = React.useState(false);
+  const [value, setValue] = React.useState(props.value);
+
+  const eventTarget = new EventTarget();
+
+  function handleEvent(event) {
+    console.log(event.type);
+  }
+
+  // React.useEffect(() => {
+  //   const onFocusOut = (event) => {
+  //     setActiveEditMode(false);
+  //   };
+  //   if (inputRef.current) {
+  //     inputRef.current.addEventListener("focusout", onFocusOut);
+  //     console.log(`we catch FOCUSOUT event`);
+  //   }
+  //   return () => {
+  //     if (inputRef.current) {
+  //       inputRef.current.removeEventListener("focusout", onFocusOut);
+  //     }
+  //   }
+  // }, []);
 
   React.useEffect(() => {
-    subRef.current.addEventListener("click", (event) => {
-      setActiveEditMode(!activeEditMode);
-    });
+    const onClick = (event) => {
+      setActiveEditMode(true);
+    };
+
+    if (subRef.current) {
+      subRef.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (subRef.current) {
+        subRef.current.removeEventListener("click", onClick);
+      }
+    }
   }, []);
 
   return (
-    activeEditMode ? <SubStartEndTimeInputField {...props} /> : <SubStartEndTime ref={subRef} {...props} />
+    activeEditMode ?
+      <SubStartEndTimeInputField
+        ref={inputRef}
+        force={props.force}
+        className={props.className}
+        value={value}
+        totalTime={props.totalTime}
+        updateValue={(value) => {
+          setValue(value);
+          setActiveEditMode(false);
+        }}
+      /> :
+      <SubStartEndTime
+        ref={subRef}
+        force={props.force}
+        className={props.className}
+        value={value}
+      />
   );
 }
 
