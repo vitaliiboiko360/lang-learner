7ec5ddf7e6d8f8586c9f7662510df0f34acdaec9
vitaliiboiko360 <vukelan@gmail.com>
2024-01-28 15:24:51 +0000

    move out anime code to separate file

--- src/text_page/text_paragraph.tsx
+++ src/text_page/text_paragraph.tsx
@@ -5,31 +5,7 @@ import { selectActiveIndex, setActiveIndexAction } from '../store/activeIndexSli
 import SubStartEndTime, { SubStartEndTimeEditableField } from './sub_start_end_time.tsx'
 import ConditionalLineBreak from './conditional_line_break.tsx'
 
-import css from './text_page.module.scss';
-
-// attributes needs to be in format {'attr':'val','attr2':'val2',...}
-function addSVGElemenReturnAnime(elementType: string, target: HTMLElement | SVGElement, attributes: Record<string, unknown> = {}, duration, to) {
-  const schema = 'http://www.w3.org/2000/svg';
-  const element: SVGElement = document.createElementNS(schema, elementType);
-  Object.entries(attributes).map(a => element.setAttribute(a[0], a[1] as string));
-  let dur = '3s'
-  if (duration) {
-    dur = duration
-  }
-
-  const animation = document.createElementNS(schema, "animate");
-  animation.setAttribute('attributeName', 'width');
-  animation.setAttribute('from', 0);
-  animation.setAttribute('to', to);
-  animation.setAttribute('begin', 'indefinite');
-  animation.setAttribute('dur', dur);
-  animation.setAttribute('repeatCount', '1');
-
-  element.appendChild(animation);
-  target.appendChild(element);
-
-  return animation;
-};
+import { cleanupSvgChildren, setupAnimation } from './anime/line_animation.ts';
 
 export default function TextParagraph(props) {
   const spanRef = React.useRef<HTMLSpanElement>(null);
@@ -38,74 +14,12 @@ export default function TextParagraph(props) {
   const dispatch = useAppDispatch();
   const selector = useAppSelector(selectActiveIndex);
 
-  function cleanupSvgChildren(svgRef) {
-    if (svgRef.current == null)
-      return;
-    while (svgRef.current.firstChild) {
-      svgRef.current.removeChild(svgRef.current.lastChild);
-    }
-  }
-
-  function setAnimation(length) {
-    const parentRect = spanRef.current.getBoundingClientRect();
-    const children = [].slice.call(spanRef.current.childNodes);
-    // console.log(`\n${JSON.stringify(parentRect)}`);
-
-    const coordinates = children.reduce((collectedArray, child) => {
-
-      const childRect = child.getBoundingClientRect();
-      const coordinateValues = { 'x': (childRect.x - parentRect.x), 'y': childRect.height * (collectedArray.length + 1), 'deltaX': childRect.width, 'lineBottomY': childRect.bottom };
-      // console.log(`word=${child.innerText}\t${JSON.stringify(childRect)}`);
-
-      const foundIndex = collectedArray.findIndex((element) => {
-        return element.lineBottomY === coordinateValues.lineBottomY;
-      });
-
-      if (foundIndex == -1) {
-        collectedArray.push(coordinateValues);
-      }
-      else {
-        collectedArray[foundIndex].deltaX = collectedArray[foundIndex].deltaX + childRect.width;
-      }
-
-      return collectedArray;
-    }, []);
-
-
-    const lineLength = coordinates.reduce((width, values) => {
-      width += values.deltaX;
-      return width;
-    }, 0);
-
-    const animationElements = coordinates.map((values) => {
-      let portionOfLength = lineLength / values.deltaX;
-      let durationOfAnimation = Math.ceil(length / portionOfLength);
-      return addSVGElemenReturnAnime('rect', svgRef.current, {
-        'x': values.x, 'y': values.y, 'stroke-width': '1', 'stroke': 'var(--blue)', 'width': '0', 'height': '1px', 'rx': '1px', 'ry': '1px'
-      }, `${durationOfAnimation}s`, values.deltaX);
-    });
-
-    const runAnimation = function (index) {
-      animationElements[index].addEventListener("endEvent", () => {
-        if (animationElements.length > (index + 1)) {
-          animationElements[index].parentNode.setAttribute('width', animationElements[index].getAttribute('to'));
-          runAnimation(index + 1);
-        }
-        else {
-          animationElements.forEach((element) => { element.parentNode.remove() });
-        }
-      });
-      animationElements[index].beginElement();
-    }
-    runAnimation(0);
-  }
-
   function onClick() {
     dispatch(setActiveIndexAction(props.index));
     if (props.index == selector) {
       cleanupSvgChildren(svgRef); // cleanup active animation
     }
-    setAnimation(props.length);
+    setupAnimation(props.length, spanRef, svgRef);
     props.onClick();
   }
 
