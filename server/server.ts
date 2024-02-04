72855660b7d6b0fa1839d011fd96e7708cd3f956
vitaliiboiko360 <vukelan@gmail.com>
2024-02-04 21:27:11 +0200

    srv ready to recieve post from front

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.json({
+  type: ['application/json', 'text/plain']
+}))
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 const SERVER_PORT = 4004;
 
 app.post('/', function (req, res) {
-  console.log('POST request to homepage');
-  console.log(req);
-  console.log(res);
+  console.log(`${req.method} request received from ${req.originalUrl} ${req.ip} : ${req.protocol}`);
+  console.log(`content type ${req.get('Content-Type')}`);
+  //console.log(`content type ${req.get('content-type')}`);
+  console.log(JSON.stringify(req.body));
+  res.sendStatus(200);
 });
 
 app.listen(SERVER_PORT);
