5b991298b0ee8d673f8e0d09f1a0c0adaeca7ea7
vitaliiboiko360 <vukelan@gmail.com>
2024-02-18 14:58:22 +0200

    adding svg test page

---
+++ src/svg_page/svg_page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import css from './svg_page.scss'
+
+
+export default function SvgPage(props) {
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <svg
+        width='80ch'
+        height='auto'
+      ></svg>
+    </div>
+  );
+}
