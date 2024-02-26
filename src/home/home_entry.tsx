c7146850cf597334ccadceefee3e733f072e24f4
vitaliiboiko360 <vukelan@gmail.com>
2024-02-26 11:46:33 +0000

    rework home style

--- src/home/home_entry.tsx
+++ src/home/home_entry.tsx
@@ -4,37 +4,16 @@ import css from './home.module.scss';
 import { makeUrlToArtwork, makeUrlToResource } from '../etc/util.ts'
 import { Link } from 'react-router-dom';
 
-export default function HomeEntry({ element, num }) {
+export default function HomeEntry({ element, height }) {
   const href = makeUrlToResource(element.resource);
   const { title, artwork } = element;
 
-  return (<div className={css.item}>
+  return (<div style={{height}} className={css.item}>
     <Link to={href} className={css.link}>
-      <div className={css.linkContent}>
-        <span className={css.image}>
-          <img src={makeUrlToArtwork(artwork)} />
-        </span>
-        <svg
-          className={css.svgShape}
-          viewBox="0 0 100 100"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill={'none'}
-            id={`${num}circlePath`}
-            d="
-              M 10, 50
-              a 40,40 0 1,1 80,0
-              40,40 0 1,1 -80,0
-            "
-          />
-          <text fontSize={'0.55rem'}>
-            <textPath href={`#${num}circlePath`}>
-              {title}
-            </textPath>
-          </text>
-        </svg>
-      </div>
+      <h6 className={css.title}>{title}</h6>
+      <span className={css.image}>
+        <img src={makeUrlToArtwork(artwork)} />
+      </span>
     </Link>
   </div>);
 }
