b05be466c7cc9c3a1739efe5cd2c9e5a98ee4586
vitaliiboiko360 <vukelan@gmail.com>
2024-11-29 11:45:39 +0200

    cleanup deps pt2

--- src/audio/audio_and_silider.tsx
+++ src/audio/audio_and_silider.tsx
@@ -1,51 +1,10 @@
 import React from 'react';
 
 import css from './audio.module.scss';
-import { toFixed } from '../etc/util.ts';
 
 const AudioAndSlider = React.forwardRef((props, audioRef) => {
   const [totalTime, setTotalTime] = React.useState(0);
   const [currentTime, setCurrentTime] = React.useState(0);
-
-  // React.useEffect(() => {
-  //   const onLoadedMetadata = () => {
-  //     // const totalTime = toFixed(audioRef.current.duration, 2);
-  //     console.log(`audioRef.current.duration ${audioRef.current?.duration}`);
-  //   }
-
-  //   audioRef.current.addEventListener("loadedmetadata", onLoadedMetadata, false);
-
-  //   return () => {
-  //     if (!audioRef.current)
-  //       return;
-  //     audioRef.current.removeEventListener("loadedmetadata", onLoadedMetadata, false);
-  //   };
-  // }, []);
-
-  // React.useEffect(() => {
-
-  //   const onTimeUpdateHandler = () => {
-  //     if (!audioRef.current) {
-  //       return;
-  //     }
-  //     const currentTime = audioRef.current.currentTime;
-  //     setCurrentTime(toFixed(currentTime, 2));
-  //   }
-
-  //   audioRef.current.addEventListener("timeupdate", onTimeUpdateHandler, false);
-
-  //   return () => {
-  //     if (!audioRef.current)
-  //       return;
-  //     audioRef.current.removeEventListener("timeupdate", onTimeUpdateHandler, false);
-  //   };
-  // }, []);
-
-  // React.useEffect(() => {
-  //   if (totalTime > 0) {
-  //     props.updateTotalTime(totalTime);
-  //   }
-  // });
 
   const lineRef = React.useRef();
 
@@ -59,17 +18,6 @@ const AudioAndSlider = React.forwardRef((props, audioRef) => {
         <span ref={lineRef} className={css.bottomLine}>
           <span></span>
         </span>
-        {/* <div style={{ display: 'none' }}>
-        <Audio ref={audioRef} audio={props.audio} />
-        <PlayPauseButton
-          ref={audioRef}
-        />
-        <SliderAudioPlayseek
-          ref={audioRef}
-          currentTime={currentTime}
-          totalTime={totalTime}
-        />
-      </div> */}
       </div>
     </>
   );
--- src/svg_page/svg_page.tsx
+++ src/svg_page/svg_page.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import css from './svg_page.scss'
+import css from './svg_page.scss';
 
-import {
-  useLoaderData,
-} from "react-router-dom";
-import { fontSize } from '@mui/system';
-
-
+import { useLoaderData } from 'react-router-dom';
 
 export default function SvgPage(props) {
-
   const svgRef = React.useRef(null);
   const data = useLoaderData();
 
@@ -25,14 +19,26 @@ export default function SvgPage(props) {
 
   let textElements = textLines.map((line, index) => {
     let yPos = `${(index + 2) * 2}em`;
-    return (<text className='text' key={index + 1} y={yPos} > {line}</text >);
+    return (
+      <text className='text' key={index + 1} y={yPos}>
+        {' '}
+        {line}
+      </text>
+    );
   });
 
   let height = `${lines.length + 1}em`;
   console.log(`height ${height}`);
 
-  let parentWidth = React.useRef(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0));
-  let parentHeight = React.useRef(Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0));
+  let parentWidth = React.useRef(
+    Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+  );
+  let parentHeight = React.useRef(
+    Math.max(
+      document.documentElement.clientHeight || 0,
+      window.innerHeight || 0
+    )
+  );
   let pRef = React.useRef(null);
 
   React.useEffect(() => {
@@ -44,13 +50,12 @@ export default function SvgPage(props) {
       // console.log(`parentWidth:${parentWidth.current}<br />parentHeight:${parentHeight.current}`);
       pRef.current.innerText = `parentWidth:${parentWidth.current}\nparentHeight:${parentHeight.current}`;
 
-      if (!svgRef.current)
-        return;
+      if (!svgRef.current) return;
 
       var bbox = svgRef.current.getBBox();
       // Update the width and height using the size of the contents
       // svgRef.current.setAttribute("width", bbox.x + bbox.width + bbox.x);
-      svgRef.current.setAttribute("height", bbox.y + bbox.height + bbox.y);
+      svgRef.current.setAttribute('height', bbox.y + bbox.height + bbox.y);
     };
     window.addEventListener('resize', onResize);
     return () => {
@@ -60,23 +65,32 @@ export default function SvgPage(props) {
 
   return (
     <div style={{ margin: '0 auto', textAlign: 'center', maxWidth: '80ch' }}>
-      <svg ref={svgRef} style={{
-        border: 'blue 1px solid',
-        width: '100%',
-        height: 'auto'
-      }}
+      <svg
+        ref={svgRef}
+        style={{
+          border: 'blue 1px solid',
+          width: '100%',
+          height: 'auto',
+        }}
         width='80ch'
         height={height}
       >
         <style>
-          .text {`
+          .text{' '}
+          {`
             font: '1.6em'
           `}
         </style>
-        <text className='text' key={0} y='2em'>{textHeader}</text>
+        <text className='text' key={0} y='2em'>
+          {textHeader}
+        </text>
         {textElements}
       </svg>
-      <p ref={pRef} >parentWidth:{parentWidth.current}<br />parentHeight:{parentHeight.current}</p>
+      <p ref={pRef}>
+        parentWidth:{parentWidth.current}
+        <br />
+        parentHeight:{parentHeight.current}
+      </p>
     </div>
   );
 }
