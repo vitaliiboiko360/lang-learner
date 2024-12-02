683d7127ad3582f5c0e4d5c0d442a6cdd5596565
vitaliiboiko360 <vukelan@gmail.com>
2024-12-02 21:37:38 +0200

    disable button submit

--- src/audio/audio_textlines.tsx
+++ src/audio/audio_textlines.tsx
@@ -3,16 +3,13 @@ import React from 'react';
 import TextLines from '../text_page/text_lines.tsx';
 import AudioAndSlider from './audio_and_silider.tsx';
 import BackHomeButton from '../various_controls/back_home_button.tsx';
-import ButtonSubmit_AudioTextSyncTime from '../various_controls/button_submit_audio_text_sync_time.tsx'
+// import ButtonSubmit_AudioTextSyncTime from '../various_controls/button_submit_audio_text_sync_time.tsx'
 
-import {
-  useLoaderData,
-} from "react-router-dom";
+import { useLoaderData } from 'react-router-dom';
 import PlaybackRateDropdown from '../various_controls/playback_rate.tsx';
 import css from './audio.module.scss';
 
 export default function AudioTextLines() {
-
   const audioRef = React.useRef(null);
   const containerRef = React.useRef(null);
   const onTimeUpdateHandler = React.useRef(null);
@@ -20,21 +17,32 @@ export default function AudioTextLines() {
   const [totalTime, setTotalTime] = React.useState(0);
 
   const updateStopTimeAudio = function (endTime) {
-
     if (onTimeUpdateHandler.current) {
-      audioRef.current.removeEventListener("timeupdate", onTimeUpdateHandler.current, false);
+      audioRef.current.removeEventListener(
+        'timeupdate',
+        onTimeUpdateHandler.current,
+        false
+      );
     }
 
     const onTimeUpdateHandlerNew = () => {
       if (audioRef.current.currentTime >= parseFloat(endTime)) {
         audioRef.current.pause();
-        audioRef.current.removeEventListener("timeupdate", onTimeUpdateHandler.current, false);
+        audioRef.current.removeEventListener(
+          'timeupdate',
+          onTimeUpdateHandler.current,
+          false
+        );
       }
-    }
+    };
 
-    audioRef.current.addEventListener("timeupdate", onTimeUpdateHandlerNew, false);
+    audioRef.current.addEventListener(
+      'timeupdate',
+      onTimeUpdateHandlerNew,
+      false
+    );
     onTimeUpdateHandler.current = onTimeUpdateHandlerNew;
-  }
+  };
 
   const onClickUserPlayNewStart = function (seconds, end) {
     audioRef.current.currentTime = seconds;
@@ -46,9 +54,14 @@ export default function AudioTextLines() {
 
   function getGradient() {
     const getRandColorHash = () => {
-      return `#${Array(3).fill(0).map(() => {
-        return Math.floor(Math.random() * 255).toString(16).padStart(2, "0");
-      }).join('')}`;
+      return `#${Array(3)
+        .fill(0)
+        .map(() => {
+          return Math.floor(Math.random() * 255)
+            .toString(16)
+            .padStart(2, '0');
+        })
+        .join('')}`;
     };
     let color1 = getRandColorHash();
     let color2 = getRandColorHash();
@@ -64,16 +77,14 @@ export default function AudioTextLines() {
     return () => {
       if (document.body.style.background != '')
         document.body.style.background = '';
-    }
+    };
   });
 
   React.useEffect(() => {
-    if (audioRef.current)
-      return;
+    if (audioRef.current) return;
     (async () => {
       const src = `data/${data.audio}`;
-      const blob = await fetch(src)
-        .then((resp) => resp.blob());
+      const blob = await fetch(src).then((resp) => resp.blob());
       const audio = new Audio(URL.createObjectURL(blob));
       audioRef.current = audio;
 
@@ -81,7 +92,7 @@ export default function AudioTextLines() {
         containerRef.current.append(audio);
       }
 
-      audioRef.current.addEventListener("loadedmetadata", (event) =>
+      audioRef.current.addEventListener('loadedmetadata', (event) =>
         setTotalTime(audioRef.current.duration)
       );
     })();
@@ -90,10 +101,14 @@ export default function AudioTextLines() {
   return (
     <div ref={containerRef} className={css.container}>
       <div className={css.controlTopPanel}>
-        <div><BackHomeButton /></div>
-        <ButtonSubmit_AudioTextSyncTime
-          ref={refArrayAudioTimeTextSync} />
-        <div><PlaybackRateDropdown ref={audioRef} /></div>
+        <div>
+          <BackHomeButton />
+        </div>
+        {/* <ButtonSubmit_AudioTextSyncTime
+          ref={refArrayAudioTimeTextSync} /> */}
+        <div>
+          <PlaybackRateDropdown ref={audioRef} />
+        </div>
       </div>
       <div className={css.page}>
         <div className={css.content}>
@@ -104,9 +119,7 @@ export default function AudioTextLines() {
           />
         </div>
       </div>
-      <AudioAndSlider
-        updateTotalTime={setTotalTime}
-      />
+      <AudioAndSlider updateTotalTime={setTotalTime} />
     </div>
   );
 }
